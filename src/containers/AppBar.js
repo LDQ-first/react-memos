1f4efefca3b07edde21b561ceca01f205c17da56
LDQ-first <178848890@qq.com>
2017-06-25 16:40:47 +0800

    optimize appbar

--- src/containers/AppBar.js
+++ src/containers/AppBar.js
@@ -7,23 +7,34 @@ import TopBar from '../styled/TopBar'
 import NavButton from '../styled/NavButton'
 import Logged from '../styled/Logged'
 import * as actions from '../actions'
+import { getIsFetching, getCurrentUser } from '../reducers'
+
+const Login = (props) => {
+  return (
+    <FlatButton {...props} label='Login' />
+  )
+}
 
 
-
-const AppBar = ({toggleSideBar}) => (
+const AppBar = ({toggleSideBar, isFetching, isLogged}) => (
     <div className='todo-appbar'>
+        {isFetching
+            ? (<ProgressBar />) : ''
+        }
         <TopBar 
             title='react-memos'
             iconElementLeft={
                 <NavButton onClick={toggleSideBar}/>
             }
+            iconElementRight={isLogged ? <Logged username={isLogged.attributes.username} /> : <Login />}
         />
     </div>
 )
 
 /*plain object*/
 const mapStateToProps = (state, {match}) => ({
-    
+    isFetching: getIsFetching(state, match.params.filter || 'all'),
+    isLogged: getCurrentUser(state)
 })
 
 export default withRouter(
